a63ee4f01de2e2510b678fd268b12acba6253f4f
csgvs <csgvs@DESKTOP-8RNI952>
2024-02-22 15:15:06 +0530

    Array code Commited

---
+++ TestingGIT/src/Java/LastIndex.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class LastIndex {
+
+	public static void main(String[] args) {
+		 int a[];
+		 a= new int[]{1,7,9,67,55,44,77};
+		int arry = a.length;
+		int index = a.length-1;
+		System.out.println(arry);
+		System.out.println(index);
+		 System.out.println(a[index-1]);
+		 
+
+	}
+
+}
